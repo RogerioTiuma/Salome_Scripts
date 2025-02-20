d997e025e18303eec3fca0c89cabc6e187dc8983
RogerioTiuma <rogeriotiuma@hotmail.com>
2025-02-20 04:00:22 -0300

    suavizacao

---
+++ Manual_Models/03_SUAVIZACAO/Exemplo_Suavizacao.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+###
+### This file is generated automatically by SALOME v9.10.0 with dump python functionality
+###
+
+import sys
+import salome
+
+salome.salome_init()
+import salome_notebook
+notebook = salome_notebook.NoteBook()
+sys.path.insert(0, r'D:/00_MODELOS/GITHUB/Salome_Scripts/Manual_Models/03_SUAVIZACAO')
+
+###
+### GEOM component
+###
+
+import GEOM
+from salome.geom import geomBuilder
+import math
+import SALOMEDS
+
+
+geompy = geomBuilder.New()
+
+O = geompy.MakeVertex(0, 0, 0)
+OX = geompy.MakeVectorDXDYDZ(1, 0, 0)
+OY = geompy.MakeVectorDXDYDZ(0, 1, 0)
+OZ = geompy.MakeVectorDXDYDZ(0, 0, 1)
